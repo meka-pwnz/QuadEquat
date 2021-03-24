ff372974a0cded520a16fa8156ac463d59ba31d0
Николай Кручинин <kruch.nik.alex@gmail.com>
2021-03-24 16:31:42 +0700

    Реализация метода половинного деления

--- QuadEquat/QuadEquat.cpp
+++ QuadEquat/QuadEquat.cpp
@@ -10,16 +10,36 @@ double quad_equat(double x, double i, double j, double k) {
 double hord_method(double a, double b, double eps, double i, double j, double k) {
     while (fabs(b - a) > eps) {
         b = a - (quad_equat(a, i, j, k) * (a - b)) / (quad_equat(a, i, j, k) - quad_equat(b, i, j, k));
-        std::cout << "b: " << b << std::endl;
+        //std::cout << "b: " << b << std::endl;
         a = b - (quad_equat(b, i, j, k) * (b - a)) / (quad_equat(b, i, j, k) - quad_equat(a, i, j, k));
-        std::cout << "a: " << a << std::endl;
+        //std::cout << "a: " << a << std::endl;
     }
     return a;
 }
 
+//Метод половинного деления
+double pol_del_method(double a, double b, double eps, double i, double j, double k) {
+    double result;
+    while (fabs(b - a) > eps) {
+        double c = (a + b) / 2;
+        if (quad_equat(c, i, j, k) * quad_equat(a, i, j, k) < 0) {
+            b = c;
+            result = b;
+        }
+        else {
+            a = c;
+            result = b;
+        }
+    }
+    return result;
+}
+
+
 int main()
 {
     //double a, b, c, eps, x1, x2;
-    double i = -2, j = 3, k = 1, eps = 0.00001, a = 0, b = 2; //Коэффициенты квадратного уравнения i, j, k; Точность eps; Интервал (a ; b);
-    std::cout << hord_method(a, b, eps, i, j, k); //Результат метода Ходр
+    double i = -2, j = 3, k = 1, eps = 0.00001, a = -2, b = 2; //Коэффициенты квадратного уравнения i, j, k; Точность eps; Интервал (a ; b);
+    std::cout << hord_method(a, b, eps, i, j, k); //Результат метода ходр
+
+    std::cout << pol_del_method(a, b, eps, i, j, k); //Результат метода половинного деления
 }
